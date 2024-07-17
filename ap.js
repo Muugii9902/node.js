1ed6cac776ac62817d4f50c259d5ff2aa785043c
Muugii9902 <muugii9902@gmail.com>
2024-07-17 12:32:36 +0800

    node.js

--- ap.js
+++ ap.js
@@ -1,16 +1,18 @@
-// es6
-const a = 100;
-const b = 200;
-const c = a + b;
+// // es6
+// const a = 100;
+// const b = 200;
+// const c = a + b;
 
-console.log("ansver:", c);
-// es6 function
-const printf = () => {};
-// module
+// console.log("ansver:", c);
+// // es6 function
+// const printf = () => {};
+// // module
 
-console.log("add", add(10, 30));
-console.log("sub", sub(10, 30));
+// console.log("add", add(10, 30));
+// console.log("sub", sub(10, 30));
 
-// module => import ||
-const add = require("./math");
-const { sub } = require("./math");
+// // module => import ||
+// const add = require("./math");
+// const { sub } = require("./math");
+import chalk from "chalk";
+console.log(chalk.blue("hello"));
